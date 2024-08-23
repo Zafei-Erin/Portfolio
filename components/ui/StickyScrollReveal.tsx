b5b4b802c4b660e25869d5b4a9e3c76a6899f098
Zafei-Erin <897659752@qq.com>
2024-08-23 11:25:14 +0800

    debug

--- components/ui/StickyScrollReveal.tsx
+++ components/ui/StickyScrollReveal.tsx
@@ -24,9 +24,12 @@ export const StickyScroll = ({
   const [shouldScroll, setShouldScroll] = useState<boolean>(false);
 
   useEffect(() => {
+    console.log(1);
     if (shouldScroll) {
+      console.log("scroll");
       ref.current.scrollIntoView({ behavior: "smooth" });
     } else {
+      console.log("set should scroll");
       setShouldScroll((prev) => !prev);
     }
   }, [shouldScroll, content]);
